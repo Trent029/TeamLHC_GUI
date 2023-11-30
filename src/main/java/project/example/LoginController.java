385c1e22e6776affa3d4096c915c9cfbd8a65b31
JadhavShrutiS <shrutisjadhav27@gmail.com>
2023-11-30 11:47:00 -0500

    login page

---
+++ src/main/java/project/example/LoginController.java
@@ -0,0 +1,17 @@
+package project.example;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class LoginController {
+
+    @FXML
+    private void btnLoginClicked() throws IOException {
+        //App.setRoot("home");
+    }
+
+    @FXML
+    private void btnSignUpClicked() throws IOException{
+        
+    }
+}
