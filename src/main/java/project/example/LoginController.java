477a590fb26f1111dbb784ae3a315f4e6879995d
JadhavShrutiS <shrutisjadhav27@gmail.com>
2023-11-30 12:10:32 -0500

    controller work

--- src/main/java/project/example/LoginController.java
+++ src/main/java/project/example/LoginController.java
@@ -2,16 +2,24 @@ package project.example;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 
 public class LoginController {
 
     @FXML
+    private Button loginButton;
+
+    @FXML
+    private Hyperlink signUpButton;
+
+    @FXML
     private void btnLoginClicked() throws IOException {
-        //App.setRoot("home");
+        //App.setRoot("home"); //go to project board of the user logged in
     }
 
     @FXML
     private void btnSignUpClicked() throws IOException{
-        App.setRoot("signUp");
+        App.setRoot("signUp"); //go to signup page
     }
 }
--- src/main/java/project/example/SignUpController.java
+++ src/main/java/project/example/SignUpController.java
@@ -2,11 +2,15 @@ package project.example;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class SignUpController {
 
     @FXML
+    private Button SignUpButton;
+
+    @FXML
     private void btnSignUpClicked() throws IOException {
-        //App.setRoot("home");
+        //App.setRoot("home"); //go to empty project board for user to create a project
     }
 }
